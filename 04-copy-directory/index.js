ee23ad8c22f67227e1e28c16e7f6d356bd8e17c3
Diana Butiakova <butyakova01@gmail.com>
2025-01-20 13:51:57 +0600

    feat(04-copy-directory): delete files that no longer exist in files folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,11 +3,21 @@ const path = require('path');
 
 const dirPath = path.resolve(__dirname, 'files');
 const newDirPath = path.resolve(__dirname, 'files-copy');
+const fileNames = Array();
 
 fs.mkdir(newDirPath, { recursive: true });
 
 fs.readdir(dirPath).then((files) => {
   files.forEach((file) => {
+    fileNames.push(file);
     fs.copyFile(path.resolve(dirPath, file), path.resolve(newDirPath, file));
+  });
+});
+
+fs.readdir(newDirPath).then((files) => {
+  files.forEach((file) => {
+    if (!fileNames.includes(file)) {
+      fs.unlink(path.resolve(newDirPath, file));
+    }
   });
 });
