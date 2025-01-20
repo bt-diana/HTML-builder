48c12bd585fc3458066fbf040e9b1615a10ff35d
Diana Butiakova <butyakova01@gmail.com>
2025-01-20 17:52:07 +0600

    feat(06-build-page): copy template.html to index.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
+const templatePagePath = path.resolve(__dirname, 'template.html');
+
 const distPath = path.resolve(__dirname, 'project-dist');
 fs.promises.mkdir(distPath, { recursive: true });
+
+const distHTMLPath = path.resolve(distPath, 'index.html');
+fs.promises.copyFile(templatePagePath, distHTMLPath);
