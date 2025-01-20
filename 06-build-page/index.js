9a97aa39888ebfc825f08f0632b2ab2611076876
Diana Butiakova <butyakova01@gmail.com>
2025-01-20 19:40:33 +0600

    feat(06-build-page): copy template.html to index.html without components templates

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,10 +1,28 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 const templatePagePath = path.resolve(__dirname, 'template.html');
+const templatePageReadStream = fs.createReadStream(templatePagePath);
 
 const distPath = path.resolve(__dirname, 'project-dist');
 fs.promises.mkdir(distPath, { recursive: true });
 
 const distHTMLPath = path.resolve(distPath, 'index.html');
-fs.promises.copyFile(templatePagePath, distHTMLPath);
+const distPageWriteStream = fs.createWriteStream(distHTMLPath);
+
+const rl = readline.createInterface(templatePageReadStream);
+
+// const templates = Array();
+
+rl.on('line', (line) => {
+  const lineTemplates = [...line.matchAll(/{{(.+?)}}/g)];
+  if (lineTemplates.length > 0) {
+    [...line.matchAll(/(?:^|}})(.*?)(?:{{|$)/g)].forEach((match) => {
+      distPageWriteStream.write(match[1]);
+    });
+  } else {
+    distPageWriteStream.write(line);
+  }
+  distPageWriteStream.write('\n');
+});
